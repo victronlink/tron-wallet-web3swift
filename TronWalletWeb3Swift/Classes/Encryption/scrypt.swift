a0aca69f6ec3388139f74f3b58d4c98450fff34a
victronlink <122434687+victronlink@users.noreply.github.com>
2026-04-16 19:46:19 +0800

    fix unsafe pointer question

--- TronWalletWeb3Swift/Classes/Encryption/scrypt.swift
+++ TronWalletWeb3Swift/Classes/Encryption/scrypt.swift
@@ -5,10 +5,13 @@ import scrypt
 public func scrypt(password: String, salt: Data, length: Int, N: Int, R: Int, P: Int) -> Data? {
     let passwordData = password.data
     var derivedKey = Data(count: length)
+    let derivedKeyCount = derivedKey.count
+    let passCount = passwordData.count
+    let saltCount = salt.count
     let status = passwordData.withUnsafeBytes { (passPtr: UnsafePointer<UInt8>) -> Int32 in
         salt.withUnsafeBytes { (saltPtr: UnsafePointer<UInt8>) -> Int32 in
             derivedKey.withUnsafeMutableBytes { (outPtr: UnsafeMutablePointer<UInt8>) -> Int32 in
-                crypto_scrypt(passPtr, passwordData.count, saltPtr, salt.count, UInt64(N), UInt32(R), UInt32(P), outPtr, derivedKey.count)
+                crypto_scrypt(passPtr, passCount, saltPtr, saltCount, UInt64(N), UInt32(R), UInt32(P), outPtr, derivedKeyCount)
             }
         }
     }
